4d4e4745f37210f95f7248a0d6175ddd663c0232
mohamedabdelmaksod87 <mohamedabdelmaksod1987@gmail.com>
2024-09-19 05:05:23 +0300

    added env support

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import "dotenv/config";
 import express from "express";
 import { CreateTeaRequest, UpdateTeaRequest, Tea } from "./types";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
